0ce279fc4a713d23bea4bf6744c19aab63dcbd83
Matheus Chong <matheus.chong@gmail.com>
2022-02-16 17:35:17 -0300

    Screen 3.1 - HTML and CSS done, JS in progress

--- script/script.js
+++ script/script.js
@@ -1,3 +1,16 @@
 const BUZZ_QUIZZ_API = "https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes";
 
+function stringSize(string,min,max){
+    if(string===null)return false;
+    if(string.length < min || string.length > max) return false;
+    return true;
+}
 
+function numberRange(number, min, max){
+    let regex = /^\d{1,}$/
+    if(!regex.test(min) || !regex.test(max)) return console.error("Invalid Parameter on numberRange Function")
+    if(!regex.test(number)) return false;
+    number = parseInt(number)
+    if (number < min || number > max) return false;
+    return true;
+}
